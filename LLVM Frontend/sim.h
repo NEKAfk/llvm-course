77a9c0a576b071f18a3fdff30d7cac8a57606131
Nikita <73298893+NEKAfk@users.noreply.github.com>
2026-04-14 22:37:01 +0300

    Reorder function declarations in sim.h

--- LLVM Frontend/sim.h
+++ LLVM Frontend/sim.h
@@ -3,10 +3,11 @@
 #define SIM_Y_SIZE 256
 
 #ifndef __icd10b69__
-void simInit();
-void app();
-void simExit();
 void flush();
 void putPixel(int x, int y, int argb);
 int randMod(int m);
 #endif
+
+void simInit();
+void app();
+void simExit();
